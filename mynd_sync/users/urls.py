4c73169f48e10b0e110fbe9072b3521585d5577e
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2024-09-27 20:19:15 -0300

    Login and Signup system

--- mynd_sync/users/urls.py
+++ mynd_sync/users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/cliente/', views.signup_cliente, name='signup_cliente'),
     path('signup/medico/', views.signup_medico, name='signup_medico'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- mynd_sync/users/views.py
+++ mynd_sync/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from .forms import ClienteSignUpForm, MedicoSignUpForm
 from .models import Usuario
+from django.contrib import messages
 
 
 # Create your views here.
@@ -30,5 +31,22 @@ def signup_medico(request):
 
 
 def login_view(request):
-    # Utilize a view de login padrão do Django ou crie sua própria view
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Bem-vindo(a), {user.username}!')
+            return redirect('home')  # Redireciona para a página principal ou qualquer outra página
+        else:
+            messages.error(request, 'Usuário ou senha incorretos. Tente novamente.')
+
+    return render(request, 'users/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Você foi desconectado com sucesso.')
+    return redirect('login')
